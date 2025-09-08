9cec86c27d54a088756ab4bdb830841876c4d0b8
Muiz-Isman <muizisman511@gmail.com>
2025-09-08 10:25:50 +0700

    Commit No. 4 – Standard activity and single top activity

--- app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
@@ -2,46 +2,37 @@ package com.example.lab_week_02_c
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import android.util.Log
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button
-import android.util.Log
 
 
-class MainActivity : AppCompatActivity() {
+class StandardActivity : AppCompatActivity() {
 
-    companion object {
+    companion object{
         private const val DEBUG = "DEBUG"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_standard)
         Log.d(DEBUG, "onCreate")
+        findViewById<Button>(R.id.button_standard).setOnClickListener { startActivity(Intent(this,
+            StandardActivity::class.java)) }
 
-        val buttonClickListener = View.OnClickListener {
-            view -> when (view.id){
-                R.id.button_standard -> startActivity(
-                    Intent(this, StandardActivity::class.java)
-                )
-            R.id.button_single_top -> startActivity(Intent(this, SingleTopActivity::class.java))
-
-            }
-        }
-        findViewById<Button>(R.id.button_standard).setOnClickListener(buttonClickListener)
-        findViewById<Button>(R.id.button_single_top).setOnClickListener(buttonClickListener)
-
-
-
-        setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onNewIntent(intent: Intent ){
+        super.onNewIntent(intent)
+        Log.d(DEBUG, "onNewIntent")
     }
 }
